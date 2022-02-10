0b16522218b2d8d911c56ca30b72e9cd321703ee
SameButDifferent <76499872+samedifferent@users.noreply.github.com>
2022-02-09 23:39:06 -0500

    Add snail json placeholders

---
+++ src/main/java/crispytwig/naturalist/entity/Snail.java
@@ -0,0 +1,20 @@
+package crispytwig.naturalist.entity;
+
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.AgeableMob;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+public class Snail extends Animal {
+    protected Snail(EntityType<? extends Animal> type, Level level) {
+        super(type, level);
+    }
+
+    @Nullable
+    @Override
+    public AgeableMob getBreedOffspring(ServerLevel p_146743_, AgeableMob p_146744_) {
+        return null;
+    }
+}
