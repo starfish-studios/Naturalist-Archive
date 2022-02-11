6c07bbf2b97f154abbf9b3f07332be18fbbdc702
crispytwig <48872606+crispytwig@users.noreply.github.com>
2022-02-11 12:24:05 -0600

    Crocodile AI but it's broken :wah:

--- src/main/java/crispytwig/naturalist/entity/Crocodile.java
+++ src/main/java/crispytwig/naturalist/entity/Crocodile.java
@@ -1,16 +1,27 @@
 package crispytwig.naturalist.entity;
 
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.AgeableMob;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.Mob;
+import net.minecraft.util.Mth;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.control.MoveControl;
 import net.minecraft.world.entity.ai.goal.*;
+import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.animal.IronGolem;
+import net.minecraft.world.entity.animal.Turtle;
+import net.minecraft.world.entity.monster.ZombifiedPiglin;
+import net.minecraft.world.entity.npc.AbstractVillager;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.pathfinder.BlockPathTypes;
+import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
@@ -20,11 +31,15 @@ import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
 
+import java.util.List;
+
 public class Crocodile extends Animal implements IAnimatable {
     private final AnimationFactory factory = new AnimationFactory(this);
 
-    public Crocodile(EntityType<? extends Animal> type, Level level) {
-        super(type, level);
+    public Crocodile(EntityType<Crocodile> type, Level world) {
+        super(type, world);
+        xpReward = 0;
+        setNoAi(false);
     }
 
     public static AttributeSupplier.Builder createAttributes() {
@@ -35,14 +50,38 @@ public class Crocodile extends Animal implements IAnimatable {
     protected void registerGoals() {
         super.registerGoals();
         this.goalSelector.addGoal(0, new FloatGoal(this));
-        this.goalSelector.addGoal(1, new RandomStrollGoal(this, 1.0D));
-        this.goalSelector.addGoal(2, new LookAtPlayerGoal(this, Player.class, 6.0F));
+        this.goalSelector.addGoal(1, new RandomStrollGoal(this, 1));
+        this.goalSelector.addGoal(2, new TemptGoal(this, 1, Ingredient.of(Items.CHICKEN, Items.RABBIT), false));
         this.goalSelector.addGoal(3, new RandomLookAroundGoal(this));
+        this.addBehaviourGoals();
+    }
+
+    protected void addBehaviourGoals() {
+        this.goalSelector.addGoal(4, new MeleeAttackGoal(this, 1.0D, false));
+        this.targetSelector.addGoal(5, new NearestAttackableTargetGoal<>(this, Player.class, true));
+        this.targetSelector.addGoal(6, new NearestAttackableTargetGoal(this, AgeableMob.class, true));
+    }
+
+        // Essentially just makes it hurt more by the Impaling enchantment //
+    @Override
+    public MobType getMobType() {
+        return MobType.WATER;
     }
 
     @Override
-    protected float getWaterSlowDown() {
-        return 0.98F;
+    public boolean canBreatheUnderwater() {
+        return true;
+    }
+
+    @Override
+    public boolean isPushedByFluid() {
+        return false;
+    }
+
+        // Breeding Food //
+    @Override
+    public boolean isFood(ItemStack stack) {
+        return List.of(Items.CHICKEN).contains(stack);
     }
 
     @Nullable
@@ -51,20 +90,17 @@ public class Crocodile extends Animal implements IAnimatable {
         return null;
     }
 
-    @Override
-    public boolean isFood(ItemStack pStack) {
-        return false;
-    }
-
     private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event) {
         if (event.isMoving()) {
             if (!this.isInWater()) {
-                event.getController().setAnimation(new AnimationBuilder().addAnimation("crocodile.walk", true));
+                event.getController().setAnimation(new AnimationBuilder()
+                        .addAnimation("crocodile.walk", true));
             } else {
-                event.getController().setAnimation(new AnimationBuilder().addAnimation("crocodile.swim", true));
+                event.getController().setAnimation(new AnimationBuilder()
+                        .addAnimation("crocodile.swim", true));
             }
-            return PlayState.CONTINUE;
-        }
+                return PlayState.CONTINUE;
+            }
         event.getController().markNeedsReload();
         return PlayState.STOP;
     }
@@ -78,5 +114,9 @@ public class Crocodile extends Animal implements IAnimatable {
     @Override
     public AnimationFactory getFactory() {
         return factory;
+    }
+
+    public Vec3 getLeashOffset() {
+        return new Vec3(0.0D, 0.6F * this.getEyeHeight(), this.getBbWidth() * 0.4F);
     }
 }
