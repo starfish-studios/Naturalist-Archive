31bd6193ed7b68cbf6bf9f1cce1ccb9067e59c8d
SameButDifferent <76499872+samedifferent@users.noreply.github.com>
2022-05-07 13:23:56 -0400

    Add eating and sitting

--- src/main/java/crispytwig/naturalist/entity/Bear.java
+++ src/main/java/crispytwig/naturalist/entity/Bear.java
@@ -4,6 +4,8 @@ import crispytwig.naturalist.entity.ai.goal.CloseMeleeAttackGoal;
 import crispytwig.naturalist.registry.NaturalistEntityTypes;
 import crispytwig.naturalist.registry.NaturalistTags;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ItemParticleOption;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
@@ -22,6 +24,7 @@ import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.ai.goal.target.ResetUniversalAngerTargetGoal;
 import net.minecraft.world.entity.animal.*;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -30,6 +33,8 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.event.ForgeEventFactory;
 import software.bernie.geckolib3.core.AnimationState;
 import software.bernie.geckolib3.core.IAnimatable;
@@ -42,6 +47,7 @@ import software.bernie.geckolib3.core.manager.AnimationFactory;
 
 import javax.annotation.Nullable;
 import java.util.EnumSet;
+import java.util.List;
 import java.util.UUID;
 import java.util.function.Predicate;
 
@@ -50,6 +56,8 @@ public class Bear extends Animal implements NeutralMob, IAnimatable {
     private static final Ingredient FOOD_ITEMS = Ingredient.of(NaturalistTags.Items.BEAR_TEMPT_ITEMS);
     private static final EntityDataAccessor<Boolean> SLEEPING = SynchedEntityData.defineId(Bear.class, EntityDataSerializers.BOOLEAN);
     private static final EntityDataAccessor<Boolean> SNIFFING = SynchedEntityData.defineId(Bear.class, EntityDataSerializers.BOOLEAN);
+    private static final EntityDataAccessor<Boolean> SITTING = SynchedEntityData.defineId(Bear.class, EntityDataSerializers.BOOLEAN);
+    private static final EntityDataAccessor<Integer> EAT_COUNTER = SynchedEntityData.defineId(Bear.class, EntityDataSerializers.INT);
     private static final UniformInt PERSISTENT_ANGER_TIME = TimeUtil.rangeOfSeconds(20, 39);
     private static final EntityDataAccessor<Integer> REMAINING_ANGER_TIME = SynchedEntityData.defineId(Bear.class, EntityDataSerializers.INT);
     @Nullable
@@ -58,7 +66,12 @@ public class Bear extends Animal implements NeutralMob, IAnimatable {
     public Bear(EntityType<? extends Animal> entityType, Level level) {
         super(entityType, level);
         this.maxUpStep = 1.0F;
+        if (!this.isBaby()) {
+            this.setCanPickUpLoot(true);
+        }
     }
+
+    // BREEDING
 
     @Nullable
     @Override
@@ -71,6 +84,8 @@ public class Bear extends Animal implements NeutralMob, IAnimatable {
         return FOOD_ITEMS.test(pStack);
     }
 
+    // ATTRIBUTES/GOALS/LOGIC
+
     public static AttributeSupplier.Builder createAttributes() {
         return Mob.createMobAttributes().add(Attributes.MAX_HEALTH, 30.0D).add(Attributes.FOLLOW_RANGE, 20.0D).add(Attributes.MOVEMENT_SPEED, 0.25D).add(Attributes.ATTACK_DAMAGE, 6.0D).add(Attributes.KNOCKBACK_RESISTANCE, 0.6D);
     }
@@ -81,6 +96,7 @@ public class Bear extends Animal implements NeutralMob, IAnimatable {
         this.goalSelector.addGoal(0, new BearFloatGoal(this));
         this.goalSelector.addGoal(1, new BreedGoal(this, 1.0D));
         this.goalSelector.addGoal(2, new BearTemptGoal(this, 1.0D, FOOD_ITEMS, false));
+        this.goalSelector.addGoal(2, new BearPickupFoodAndSitGoal(this));
         this.goalSelector.addGoal(3, new CloseMeleeAttackGoal(this, 1.25D, true));
         this.goalSelector.addGoal(3, new BearPanicGoal(this, 2.0D));
         this.goalSelector.addGoal(4, new BearSleepGoal(this));
@@ -96,6 +112,27 @@ public class Bear extends Animal implements NeutralMob, IAnimatable {
         this.targetSelector.addGoal(5, new ResetUniversalAngerTargetGoal<>(this, false));
     }
 
+    @Override
+    public void aiStep() {
+        super.aiStep();
+        if (!this.level.isClientSide) {
+            this.updatePersistentAnger((ServerLevel)this.level, true);
+        }
+        if (this.isSleeping() || this.isImmobile()) {
+            this.jumping = false;
+            this.xxa = 0.0F;
+            this.zza = 0.0F;
+        }
+        if (this.getTarget() != null) {
+            if (this.getTarget() instanceof Player player) {
+                if (FOOD_ITEMS.test(player.getMainHandItem()) || FOOD_ITEMS.test(player.getOffhandItem())) {
+                    this.stopBeingAngry();
+                }
+            }
+        }
+        this.handleEating();
+    }
+
     // ENTITY DATA
 
     @Override
@@ -103,6 +140,8 @@ public class Bear extends Animal implements NeutralMob, IAnimatable {
         super.defineSynchedData();
         this.entityData.define(SLEEPING, false);
         this.entityData.define(SNIFFING, false);
+        this.entityData.define(SITTING, false);
+        this.entityData.define(EAT_COUNTER, 0);
         this.entityData.define(REMAINING_ANGER_TIME, 0);
     }
 
@@ -135,6 +174,32 @@ public class Bear extends Animal implements NeutralMob, IAnimatable {
         this.entityData.set(SNIFFING, sniffing);
     }
 
+    public boolean isSitting() {
+        return this.entityData.get(SITTING);
+    }
+
+    public void setSitting(boolean sitting) {
+        this.entityData.set(SITTING, sitting);
+    }
+
+    public boolean isEating() {
+        return this.entityData.get(EAT_COUNTER) > 0;
+    }
+
+    public void eat(boolean eat) {
+        this.entityData.set(EAT_COUNTER, eat ? 1 : 0);
+    }
+
+    private int getEatCounter() {
+        return this.entityData.get(EAT_COUNTER);
+    }
+
+    private void setEatCounter(int amount) {
+        this.entityData.set(EAT_COUNTER, amount);
+    }
+
+    // ANGER
+
     @Override
     public void startPersistentAngerTimer() {
         this.setRemainingPersistentAngerTime(PERSISTENT_ANGER_TIME.sample(this.random));
@@ -161,39 +226,98 @@ public class Bear extends Animal implements NeutralMob, IAnimatable {
         return this.persistentAngerTarget;
     }
 
-    @Override
-    public void aiStep() {
-        super.aiStep();
-        if (!this.level.isClientSide) {
-            this.updatePersistentAnger((ServerLevel)this.level, true);
+    // EATING
+
+    private void handleEating() {
+        if (!this.isEating() && this.isSitting() && !this.isSleeping() && !this.getMainHandItem().isEmpty() && this.random.nextInt(80) == 1) {
+            this.eat(true);
+        } else if (this.getMainHandItem().isEmpty() || !this.isSitting()) {
+            this.eat(false);
         }
-        if (this.isSleeping() || this.isImmobile()) {
-            this.jumping = false;
-            this.xxa = 0.0F;
-            this.zza = 0.0F;
-        }
-        if (this.getTarget() != null) {
-            if (this.getTarget() instanceof Player player) {
-                if (FOOD_ITEMS.test(player.getMainHandItem()) || FOOD_ITEMS.test(player.getOffhandItem())) {
-                    this.stopBeingAngry();
+        if (this.isEating()) {
+            this.addEatingParticles();
+            if (!this.level.isClientSide && this.getEatCounter() > 80 && this.random.nextInt(20) == 1) {
+                if (this.getEatCounter() > 100 && this.isFood(this.getItemBySlot(EquipmentSlot.MAINHAND))) {
+                    if (!this.level.isClientSide) {
+                        this.setItemSlot(EquipmentSlot.MAINHAND, ItemStack.EMPTY);
+                        this.gameEvent(GameEvent.EAT, this.eyeBlockPosition());
+                    }
+                    this.setSitting(false);
                 }
+                this.eat(false);
+                return;
+            }
+            this.setEatCounter(this.getEatCounter() + 1);
+        }
+    }
+
+    private void addEatingParticles() {
+        if (this.getEatCounter() % 5 == 0) {
+            this.playSound(SoundEvents.PANDA_EAT, 0.5F + 0.5F * (float)this.random.nextInt(2), (this.random.nextFloat() - this.random.nextFloat()) * 0.2F + 1.0F);
+
+            for(int i = 0; i < 6; ++i) {
+                Vec3 speedVec = new Vec3(((double)this.random.nextFloat() - 0.5D) * 0.1D, Math.random() * 0.1D + 0.1D, ((double)this.random.nextFloat() - 0.5D) * 0.1D);
+                speedVec  = speedVec .xRot(-this.getXRot() * ((float)Math.PI / 180F));
+                speedVec  = speedVec .yRot(-this.getYRot() * ((float)Math.PI / 180F));
+                double y = (double)(-this.random.nextFloat()) * 0.6D - 0.3D;
+                Vec3 posVec = new Vec3(((double)this.random.nextFloat() - 0.5D) * 0.8D, y, 1.0D + ((double)this.random.nextFloat() - 0.5D) * 0.4D);
+                posVec = posVec.yRot(-this.yBodyRot * ((float)Math.PI / 180F));
+                posVec = posVec.add(this.getX(), this.getEyeY() + 1.0D, this.getZ());
+                this.level.addParticle(new ItemParticleOption(ParticleTypes.ITEM, this.getItemBySlot(EquipmentSlot.MAINHAND)), posVec.x, posVec.y, posVec.z, speedVec .x, speedVec .y + 0.05D, speedVec .z);
             }
         }
     }
+
+    @Override
+    public boolean canTakeItem(ItemStack pItemstack) {
+        EquipmentSlot slot = Mob.getEquipmentSlotForItem(pItemstack);
+        if (!this.getItemBySlot(slot).isEmpty()) {
+            return false;
+        } else {
+            return slot == EquipmentSlot.MAINHAND && super.canTakeItem(pItemstack);
+        }
+    }
+
+    @Override
+    protected void pickUpItem(ItemEntity pItemEntity) {
+        ItemStack stack = pItemEntity.getItem();
+        if (this.getMainHandItem().isEmpty() && FOOD_ITEMS.test(stack)) {
+            this.onItemPickup(pItemEntity);
+            this.setItemSlot(EquipmentSlot.MAINHAND, stack);
+            this.handDropChances[EquipmentSlot.MAINHAND.getIndex()] = 2.0F;
+            this.take(pItemEntity, stack.getCount());
+            pItemEntity.discard();
+        }
+    }
+
+    // MOVEMENT
 
     @Override
     protected float getWaterSlowDown() {
         return 0.85F;
     }
 
+    void tryToSit() {
+        if (!this.isInWater()) {
+            this.setZza(0.0F);
+            this.getNavigation().stop();
+            this.setSitting(true);
+        }
+    }
+
+    // ANIMATION
+
     private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event) {
         if (this.isSleeping()) {
             event.getController().setAnimation(new AnimationBuilder().addAnimation("bear.sleep", true));
             return PlayState.CONTINUE;
+        } else if (this.isSitting()) {
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("bear.sit", true));
+            return PlayState.CONTINUE;
         } else if (event.isMoving()) {
             event.getController().setAnimation(new AnimationBuilder().addAnimation("bear.walk", true));
             return PlayState.CONTINUE;
-        } else if (!this.isSniffing()) {
+        } else if (!this.isSniffing() && !this.isSitting()) {
             event.getController().setAnimation(new AnimationBuilder().addAnimation("bear.idle", true));
             return PlayState.CONTINUE;
         }
@@ -219,18 +343,33 @@ public class Bear extends Animal implements NeutralMob, IAnimatable {
         return PlayState.CONTINUE;
     }
 
+    private <E extends IAnimatable> PlayState eatPredicate(AnimationEvent<E> event) {
+        if (this.isEating()) {
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("bear.eat", true));
+            return PlayState.CONTINUE;
+        } else if (this.isSitting()) {
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("bear.sit", true));
+            return PlayState.CONTINUE;
+        }
+        event.getController().markNeedsReload();
+        return PlayState.STOP;
+    }
+
     @Override
     public void registerControllers(AnimationData data) {
         data.setResetSpeedInTicks(10);
         data.addAnimationController(new AnimationController<>(this, "controller", 10, this::predicate));
-        data.addAnimationController(new AnimationController<>(this, "swingController", 0, this::sniffPredicate));
-        data.addAnimationController(new AnimationController<>(this, "sniffController", 0, this::swingPredicate));
+        data.addAnimationController(new AnimationController<>(this, "sniffController", 0, this::sniffPredicate));
+        data.addAnimationController(new AnimationController<>(this, "swingController", 0, this::swingPredicate));
+        data.addAnimationController(new AnimationController<>(this, "eatController", 10, this::eatPredicate));
     }
 
     @Override
     public AnimationFactory getFactory() {
         return factory;
     }
+
+    // GOALS
 
     static class BearPanicGoal extends PanicGoal {
         public BearPanicGoal(PathfinderMob pMob, double pSpeedModifier) {
@@ -471,6 +610,67 @@ public class Bear extends Animal implements NeutralMob, IAnimatable {
         public void stop() {
             super.stop();
             bear.setSniffing(false);
+        }
+    }
+
+    static class BearPickupFoodAndSitGoal extends Goal {
+        private int cooldown;
+        private final Bear bear;
+        
+        public BearPickupFoodAndSitGoal(Bear bear) {
+            this.setFlags(EnumSet.of(Goal.Flag.MOVE));
+            this.bear = bear;
+        }
+
+        @Override
+        public boolean canUse() {
+            if (this.cooldown <= bear.tickCount && !bear.isBaby() && !bear.isInWater() && !bear.isSleeping() && !bear.isSitting()) {
+                List<ItemEntity> list = bear.level.getEntitiesOfClass(ItemEntity.class, bear.getBoundingBox().inflate(6.0D, 6.0D, 6.0D), itemEntity -> FOOD_ITEMS.test(itemEntity.getItem()));
+                return !list.isEmpty() || !bear.getMainHandItem().isEmpty();
+            } else {
+                return false;
+            }
+        }
+
+        @Override
+        public boolean canContinueToUse() {
+            if (!bear.isInWater() && bear.random.nextInt(reducedTickDelay(600)) != 1) {
+                return bear.random.nextInt(reducedTickDelay(2000)) != 1;
+            } else {
+                return false;
+            }
+        }
+
+        @Override
+        public void tick() {
+            if (!bear.isSitting() && !bear.getMainHandItem().isEmpty()) {
+                bear.tryToSit();
+            }
+        }
+
+        @Override
+        public void start() {
+            List<ItemEntity> list = bear.level.getEntitiesOfClass(ItemEntity.class, bear.getBoundingBox().inflate(8.0D, 8.0D, 8.0D), itemEntity -> FOOD_ITEMS.test(itemEntity.getItem()));
+            if (!list.isEmpty() && bear.getMainHandItem().isEmpty()) {
+                bear.getNavigation().moveTo(list.get(0), 1.2F);
+            } else if (!bear.getMainHandItem().isEmpty()) {
+                bear.tryToSit();
+            }
+
+            this.cooldown = 0;
+        }
+
+        @Override
+        public void stop() {
+            ItemStack itemstack = bear.getItemBySlot(EquipmentSlot.MAINHAND);
+            if (!itemstack.isEmpty()) {
+                bear.spawnAtLocation(itemstack);
+                bear.setItemSlot(EquipmentSlot.MAINHAND, ItemStack.EMPTY);
+                int cooldownSeconds = bear.random.nextInt(150) + 10;
+                this.cooldown = bear.tickCount + cooldownSeconds * 20;
+            }
+
+            bear.setSitting(false);
         }
     }
 }
