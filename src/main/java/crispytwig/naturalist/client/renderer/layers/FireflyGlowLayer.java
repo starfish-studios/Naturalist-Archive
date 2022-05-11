bc7479633456104e40f7cf95d54ce5094c19e560
SameButDifferent <76499872+samedifferent@users.noreply.github.com>
2022-05-11 00:47:41 -0400

    Fix firefly glow layer

--- src/main/java/crispytwig/naturalist/client/renderer/layers/FireflyGlowLayer.java
+++ src/main/java/crispytwig/naturalist/client/renderer/layers/FireflyGlowLayer.java
@@ -14,7 +14,11 @@ import software.bernie.geckolib3.renderers.geo.IGeoRenderer;
 
 @OnlyIn(Dist.CLIENT)
 public class FireflyGlowLayer extends GeoLayerRenderer<Firefly> {
-    private static final ResourceLocation LAYER = new ResourceLocation(Naturalist.MOD_ID, "textures/entity/firefly/firefly_glow.png");
+    private static final ResourceLocation TOP_LAYER = new ResourceLocation(Naturalist.MOD_ID, "textures/entity/firefly/firefly_glow_top.png");
+    private static final ResourceLocation BACK_LAYER = new ResourceLocation(Naturalist.MOD_ID, "textures/entity/firefly/firefly_glow_back.png");
+    private static final ResourceLocation BOTTOM_LAYER = new ResourceLocation(Naturalist.MOD_ID, "textures/entity/firefly/firefly_glow_bottom.png");
+    private static final ResourceLocation LEFT_LAYER = new ResourceLocation(Naturalist.MOD_ID, "textures/entity/firefly/firefly_glow_left.png");
+    private static final ResourceLocation RIGHT_LAYER = new ResourceLocation(Naturalist.MOD_ID, "textures/entity/firefly/firefly_glow_right.png");
     private static final ResourceLocation MODEL = new ResourceLocation(Naturalist.MOD_ID, "geo/firefly.geo.json");
 
     public FireflyGlowLayer(IGeoRenderer<Firefly> entityRendererIn) {
@@ -23,10 +27,23 @@ public class FireflyGlowLayer extends GeoLayerRenderer<Firefly> {
 
     @Override
     public void render(PoseStack matrixStackIn, MultiBufferSource bufferIn, int packedLightIn, Firefly entityLivingBaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
-        RenderType renderType = entityLivingBaseIn.isGlowing() ? RenderType.eyes(LAYER) : RenderType.entityCutoutNoCull(LAYER);
+        RenderType top = entityLivingBaseIn.isGlowing() ? RenderType.eyes(TOP_LAYER) : RenderType.entityCutoutNoCull(TOP_LAYER);
+        RenderType back = entityLivingBaseIn.isGlowing() ? RenderType.eyes(BACK_LAYER) : RenderType.entityCutoutNoCull(BACK_LAYER);
+        RenderType bottom = entityLivingBaseIn.isGlowing() ? RenderType.eyes(BOTTOM_LAYER) : RenderType.entityCutoutNoCull(BOTTOM_LAYER);
+        RenderType left = entityLivingBaseIn.isGlowing() ? RenderType.eyes(LEFT_LAYER) : RenderType.entityCutoutNoCull(LEFT_LAYER);
+        RenderType right = entityLivingBaseIn.isGlowing() ? RenderType.eyes(RIGHT_LAYER) : RenderType.entityCutoutNoCull(RIGHT_LAYER);
+
         matrixStackIn.pushPose();
-        this.getRenderer().render(this.getEntityModel().getModel(MODEL), entityLivingBaseIn, partialTicks, renderType, matrixStackIn, bufferIn,
-                bufferIn.getBuffer(renderType), 15728640, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
+        this.getRenderer().render(this.getEntityModel().getModel(MODEL), entityLivingBaseIn, partialTicks, top, matrixStackIn, bufferIn,
+                bufferIn.getBuffer(top), packedLightIn, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
+        this.getRenderer().render(this.getEntityModel().getModel(MODEL), entityLivingBaseIn, partialTicks, back, matrixStackIn, bufferIn,
+                bufferIn.getBuffer(back), packedLightIn, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
+        this.getRenderer().render(this.getEntityModel().getModel(MODEL), entityLivingBaseIn, partialTicks, bottom, matrixStackIn, bufferIn,
+                bufferIn.getBuffer(bottom), packedLightIn, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
+        this.getRenderer().render(this.getEntityModel().getModel(MODEL), entityLivingBaseIn, partialTicks, left, matrixStackIn, bufferIn,
+                bufferIn.getBuffer(left), packedLightIn, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
+        this.getRenderer().render(this.getEntityModel().getModel(MODEL), entityLivingBaseIn, partialTicks, right, matrixStackIn, bufferIn,
+                bufferIn.getBuffer(right), packedLightIn, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
         matrixStackIn.popPose();
     }
 }
