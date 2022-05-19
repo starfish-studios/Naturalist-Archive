2d625548a2f74ff4d89cfdf900909ebb0b793649
SameButDifferent <76499872+samedifferent@users.noreply.github.com>
2022-05-19 01:26:06 -0400

    Add mob spawns

--- src/main/java/crispytwig/naturalist/event/CommonEventHandler.java
+++ src/main/java/crispytwig/naturalist/event/CommonEventHandler.java
@@ -17,6 +17,57 @@ public class CommonEventHandler {
             case FOREST -> {
                 event.getSpawns().addSpawn(MobCategory.CREATURE, new MobSpawnSettings.SpawnerData(NaturalistEntityTypes.BEAR.get(), 4, 1, 2));
                 event.getSpawns().addSpawn(MobCategory.CREATURE, new MobSpawnSettings.SpawnerData(NaturalistEntityTypes.DEER.get(), 4, 2, 4));
+                event.getSpawns().addSpawn(MobCategory.CREATURE, new MobSpawnSettings.SpawnerData(NaturalistEntityTypes.SNAKE.get(), 4, 1, 1));
+                event.getSpawns().addSpawn(MobCategory.CREATURE, new MobSpawnSettings.SpawnerData(NaturalistEntityTypes.SNAIL.get(), 4, 1, 3));
+                event.getSpawns().addSpawn(MobCategory.AMBIENT, new MobSpawnSettings.SpawnerData(NaturalistEntityTypes.FIREFLY.get(), 5, 3, 6));
+                event.getSpawns().addSpawn(MobCategory.CREATURE, new MobSpawnSettings.SpawnerData(NaturalistEntityTypes.BUTTERFLY.get(), 4, 1, 3));
+            }
+            case TAIGA -> {
+                event.getSpawns().addSpawn(MobCategory.CREATURE, new MobSpawnSettings.SpawnerData(NaturalistEntityTypes.BEAR.get(), 4, 1, 2));
+            }
+            case PLAINS -> {
+                event.getSpawns().addSpawn(MobCategory.CREATURE, new MobSpawnSettings.SpawnerData(NaturalistEntityTypes.SNAKE.get(), 4, 1, 1));
+                event.getSpawns().addSpawn(MobCategory.CREATURE, new MobSpawnSettings.SpawnerData(NaturalistEntityTypes.SNAIL.get(), 4, 1, 3));
+                event.getSpawns().addSpawn(MobCategory.AMBIENT, new MobSpawnSettings.SpawnerData(NaturalistEntityTypes.FIREFLY.get(), 5, 3, 6));
+                event.getSpawns().addSpawn(MobCategory.CREATURE, new MobSpawnSettings.SpawnerData(NaturalistEntityTypes.BUTTERFLY.get(), 4, 1, 3));
+            }
+            case SWAMP -> {
+                event.getSpawns().addSpawn(MobCategory.CREATURE, new MobSpawnSettings.SpawnerData(NaturalistEntityTypes.SNAKE.get(), 4, 1, 1));
+                event.getSpawns().addSpawn(MobCategory.CREATURE, new MobSpawnSettings.SpawnerData(NaturalistEntityTypes.SNAIL.get(), 4, 1, 3));
+                event.getSpawns().addSpawn(MobCategory.AMBIENT, new MobSpawnSettings.SpawnerData(NaturalistEntityTypes.FIREFLY.get(), 5, 3, 6));
+            }
+            case MESA -> {
+                event.getSpawns().addSpawn(MobCategory.CREATURE, new MobSpawnSettings.SpawnerData(NaturalistEntityTypes.RATTLESNAKE.get(), 4, 1, 1));
+            }
+            case SAVANNA -> {
+                event.getSpawns().addSpawn(MobCategory.CREATURE, new MobSpawnSettings.SpawnerData(NaturalistEntityTypes.RATTLESNAKE.get(), 4, 1, 1));
+                event.getSpawns().addSpawn(MobCategory.CREATURE, new MobSpawnSettings.SpawnerData(NaturalistEntityTypes.SNAIL.get(), 4, 1, 3));
+            }
+            case DESERT -> {
+                event.getSpawns().addSpawn(MobCategory.CREATURE, new MobSpawnSettings.SpawnerData(NaturalistEntityTypes.RATTLESNAKE.get(), 4, 1, 1));
+            }
+            case JUNGLE -> {
+                event.getSpawns().addSpawn(MobCategory.CREATURE, new MobSpawnSettings.SpawnerData(NaturalistEntityTypes.CORAL_SNAKE.get(), 4, 1, 1));
+            }
+            case BEACH -> {
+                event.getSpawns().addSpawn(MobCategory.CREATURE, new MobSpawnSettings.SpawnerData(NaturalistEntityTypes.CORAL_SNAKE.get(), 4, 1, 1));
+            }
+            case RIVER -> {
+                event.getSpawns().addSpawn(MobCategory.CREATURE, new MobSpawnSettings.SpawnerData(NaturalistEntityTypes.CORAL_SNAKE.get(), 4, 1, 1));
+                event.getSpawns().addSpawn(MobCategory.CREATURE, new MobSpawnSettings.SpawnerData(NaturalistEntityTypes.SNAIL.get(), 4, 1, 3));
+            }
+            case EXTREME_HILLS -> {
+                event.getSpawns().addSpawn(MobCategory.CREATURE, new MobSpawnSettings.SpawnerData(NaturalistEntityTypes.SNAIL.get(), 4, 1, 3));
+            }
+            case MUSHROOM -> {
+                event.getSpawns().addSpawn(MobCategory.CREATURE, new MobSpawnSettings.SpawnerData(NaturalistEntityTypes.SNAIL.get(), 4, 1, 3));
+                event.getSpawns().addSpawn(MobCategory.AMBIENT, new MobSpawnSettings.SpawnerData(NaturalistEntityTypes.FIREFLY.get(), 5, 3, 6));
+            }
+            case UNDERGROUND -> {
+                event.getSpawns().addSpawn(MobCategory.CREATURE, new MobSpawnSettings.SpawnerData(NaturalistEntityTypes.SNAIL.get(), 4, 1, 3));
+            }
+            case MOUNTAIN -> {
+                event.getSpawns().addSpawn(MobCategory.CREATURE, new MobSpawnSettings.SpawnerData(NaturalistEntityTypes.SNAIL.get(), 4, 1, 3));
             }
         }
     }
