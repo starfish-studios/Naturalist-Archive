cef0454366151bc4fdc75f4d259f8e0058faed52
SameButDifferent <76499872+samedifferent@users.noreply.github.com>
2022-07-04 21:10:31 -0400

    Add elephant drinking

--- common/src/main/java/com/starfish_studios/naturalist/entity/Elephant.java
+++ common/src/main/java/com/starfish_studios/naturalist/entity/Elephant.java
@@ -4,8 +4,9 @@ import com.starfish_studios.naturalist.entity.ai.goal.AttackPlayerNearBabiesGoal
 import com.starfish_studios.naturalist.entity.ai.goal.BabyHurtByTargetGoal;
 import com.starfish_studios.naturalist.entity.ai.goal.BabyPanicGoal;
 import com.starfish_studios.naturalist.entity.ai.goal.DistancedFollowParentGoal;
-import com.starfish_studios.naturalist.entity.ai.navigation.BetterGroundPathNavigation;
 import com.starfish_studios.naturalist.registry.NaturalistEntityTypes;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.core.particles.BlockParticleOption;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.nbt.CompoundTag;
@@ -24,15 +25,17 @@ import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
+import net.minecraft.world.entity.ai.navigation.GroundPathNavigation;
 import net.minecraft.world.entity.ai.navigation.PathNavigation;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.animal.Bee;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.Vec3;
-import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib3.core.AnimationState;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
@@ -42,9 +45,15 @@ import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
 
+import javax.annotation.Nullable;
+import java.util.EnumSet;
+
 public class Elephant extends Animal implements IAnimatable {
     private final AnimationFactory factory = new AnimationFactory(this);
     private static final EntityDataAccessor<Integer> DIRTY_TICKS = SynchedEntityData.defineId(Elephant.class, EntityDataSerializers.INT);
+    private static final EntityDataAccessor<Boolean> DRINKING = SynchedEntityData.defineId(Elephant.class, EntityDataSerializers.BOOLEAN);
+    @Nullable
+    protected BlockPos waterPos;
 
     public Elephant(EntityType<? extends Animal> entityType, Level level) {
         super(entityType, level);
@@ -78,7 +87,7 @@ public class Elephant extends Animal implements IAnimatable {
 
     @Override
     protected PathNavigation createNavigation(Level level) {
-        return new BetterGroundPathNavigation(this, level);
+        return new GroundPathNavigation(this, level);
     }
 
     @Override
@@ -89,9 +98,11 @@ public class Elephant extends Animal implements IAnimatable {
         this.goalSelector.addGoal(2, new ElephantMeleeAttackGoal(this, 1.2D, false));
         this.goalSelector.addGoal(3, new BabyPanicGoal(this, 2.0D));
         this.goalSelector.addGoal(4, new DistancedFollowParentGoal(this, 1.25D, 24.0D, 6.0D, 12.0D));
-        this.goalSelector.addGoal(5, new WaterAvoidingRandomStrollGoal(this, 1.0));
-        this.goalSelector.addGoal(6, new LookAtPlayerGoal(this, Player.class, 6.0f));
-        this.goalSelector.addGoal(7, new RandomLookAroundGoal(this));
+        this.goalSelector.addGoal(5, new ElephantDrinkWaterGoal(this));
+        this.goalSelector.addGoal(6, new ElephantMoveToWaterGoal(this, 1.0D, 8, 4));
+        this.goalSelector.addGoal(7, new WaterAvoidingRandomStrollGoal(this, 1.0));
+        this.goalSelector.addGoal(8, new LookAtPlayerGoal(this, Player.class, 6.0f));
+        this.goalSelector.addGoal(9, new RandomLookAroundGoal(this));
         this.targetSelector.addGoal(1, new BabyHurtByTargetGoal(this));
         this.targetSelector.addGoal(2, new AttackPlayerNearBabiesGoal(this, 0.5F));
     }
@@ -119,6 +130,7 @@ public class Elephant extends Animal implements IAnimatable {
     protected void defineSynchedData() {
         super.defineSynchedData();
         this.entityData.define(DIRTY_TICKS, 0);
+        this.entityData.define(DRINKING, false);
     }
 
     @Override
@@ -145,6 +157,14 @@ public class Elephant extends Animal implements IAnimatable {
         return this.getDirtyTicks() > 0;
     }
 
+    public void setDrinking(boolean drinking) {
+        this.entityData.set(DRINKING, drinking);
+    }
+
+    public boolean isDrinking() {
+        return this.entityData.get(DRINKING);
+    }
+
     @Override
     public void aiStep() {
         super.aiStep();
@@ -166,6 +186,8 @@ public class Elephant extends Animal implements IAnimatable {
     private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event) {
         if (this.getDeltaMovement().horizontalDistanceSqr() > 1.0E-6) {
             event.getController().setAnimation(new AnimationBuilder().addAnimation("elephant.walk", true));
+        } else if (this.isDrinking()) {
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("elephant.water", true));
         } else {
             event.getController().setAnimation(new AnimationBuilder().addAnimation("elephant.idle", true));
         }
@@ -201,6 +223,95 @@ public class Elephant extends Animal implements IAnimatable {
         @Override
         protected double getAttackReachSqr(LivingEntity attackTarget) {
             return Mth.square(this.mob.getBbWidth());
+        }
+    }
+
+    static class ElephantMoveToWaterGoal extends MoveToBlockGoal {
+        private final Elephant elephant;
+
+        public ElephantMoveToWaterGoal(Elephant pathfinderMob, double speedModifier, int searchRange, int verticalSearchRange) {
+            super(pathfinderMob, speedModifier, searchRange, verticalSearchRange);
+            this.elephant = pathfinderMob;
+        }
+
+        @Override
+        public boolean canUse() {
+            return !this.elephant.isBaby() && !this.elephant.isDrinking() && this.elephant.waterPos == null && super.canUse();
+        }
+
+        @Override
+        public boolean canContinueToUse() {
+            return !this.isReachedTarget() && super.canContinueToUse();
+        }
+
+        @Override
+        protected boolean isValidTarget(LevelReader level, BlockPos pos) {
+            if (level.getBlockState(pos).is(Blocks.GRASS_BLOCK)) {
+                for (Direction direction : Direction.Plane.HORIZONTAL) {
+                    if (level.getFluidState(pos.relative(direction)).is(Fluids.WATER)) {
+                        this.elephant.waterPos = pos.relative(direction);
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        @Override
+        public double acceptedDistance() {
+            return 2.5D;
+        }
+
+        @Override
+        public void stop() {
+            this.elephant.setDrinking(true);
+            super.stop();
+        }
+    }
+
+    static class ElephantDrinkWaterGoal extends Goal {
+        private final Elephant elephant;
+        private int drinkTicks;
+
+        public ElephantDrinkWaterGoal(Elephant elephant) {
+            this.elephant = elephant;
+            this.setFlags(EnumSet.of(Goal.Flag.MOVE, Goal.Flag.JUMP, Goal.Flag.LOOK));
+        }
+
+        @Override
+        public boolean canUse() {
+            if (this.elephant.waterPos == null || this.elephant.distanceToSqr(Vec3.atCenterOf(this.elephant.waterPos)) > 15) {
+                this.elephant.setDrinking(false);
+                return false;
+            }
+            return this.elephant.isDrinking();
+        }
+
+        @Override
+        public boolean canContinueToUse() {
+            return this.drinkTicks > 0;
+        }
+
+        @Override
+        public void start() {
+            this.drinkTicks = 150;
+            if (this.elephant.waterPos != null) {
+                this.elephant.getLookControl().setLookAt(Vec3.atCenterOf(this.elephant.waterPos));
+            }
+        }
+
+        @Override
+        public void tick() {
+            this.drinkTicks--;
+            if (this.elephant.waterPos != null) {
+                this.elephant.getLookControl().setLookAt(Vec3.atCenterOf(this.elephant.waterPos));
+            }
+        }
+
+        @Override
+        public void stop() {
+            this.elephant.waterPos = null;
+            this.elephant.setDrinking(false);
         }
     }
 }
