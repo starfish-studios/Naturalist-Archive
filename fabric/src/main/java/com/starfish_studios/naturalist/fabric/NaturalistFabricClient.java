ed1e2b3e3dea4c1f87eabd025783b6116640720d
SameButDifferent <76499872+samedifferent@users.noreply.github.com>
2022-07-13 11:37:27 -0400

    Make zebra use vanilla model

--- fabric/src/main/java/com/starfish_studios/naturalist/fabric/NaturalistFabricClient.java
+++ fabric/src/main/java/com/starfish_studios/naturalist/fabric/NaturalistFabricClient.java
@@ -1,11 +1,17 @@
 package com.starfish_studios.naturalist.fabric;
 
 import com.starfish_studios.naturalist.NaturalistClient;
+import com.starfish_studios.naturalist.client.renderer.ZebraRenderer;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
+import net.minecraft.client.model.HorseModel;
+import net.minecraft.client.model.geom.builders.CubeDeformation;
+import net.minecraft.client.model.geom.builders.LayerDefinition;
 
 public class NaturalistFabricClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         NaturalistClient.init();
+        EntityModelLayerRegistry.registerModelLayer(ZebraRenderer.LAYER_LOCATION, () -> LayerDefinition.create(HorseModel.createBodyMesh(CubeDeformation.NONE), 64, 64));
     }
 }
--- forge/src/main/java/com/starfish_studios/naturalist/forge/NaturalistForgeClient.java
+++ forge/src/main/java/com/starfish_studios/naturalist/forge/NaturalistForgeClient.java
@@ -2,6 +2,14 @@ package com.starfish_studios.naturalist.forge;
 
 import com.starfish_studios.naturalist.Naturalist;
 import com.starfish_studios.naturalist.NaturalistClient;
+import com.starfish_studios.naturalist.client.renderer.ZebraRenderer;
+import net.minecraft.client.model.BoatModel;
+import net.minecraft.client.model.HorseModel;
+import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.client.model.geom.builders.CubeDeformation;
+import net.minecraft.client.model.geom.builders.LayerDefinition;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -11,5 +19,10 @@ public class NaturalistForgeClient {
     @SubscribeEvent
     public static void init(FMLClientSetupEvent event) {
         NaturalistClient.init();
+    }
+
+    @SubscribeEvent
+    public static void registerLayerDefinitions(EntityRenderersEvent.RegisterLayerDefinitions event) {
+        event.registerLayerDefinition(ZebraRenderer.LAYER_LOCATION, () -> LayerDefinition.create(HorseModel.createBodyMesh(CubeDeformation.NONE), 64, 64));
     }
 }
