65160415f65c527c242662658e42017a01fc63d8
SameButDifferent <76499872+samedifferent@users.noreply.github.com>
2022-08-11 19:48:43 +0300

    Fix zebra passenger offset and no galloping sounds

--- common/src/main/java/com/starfish_studios/naturalist/entity/Zebra.java
+++ common/src/main/java/com/starfish_studios/naturalist/entity/Zebra.java
@@ -92,10 +92,17 @@ public class Zebra extends AbstractChestedHorse {
 
     @Override
     protected void playGallopSound(SoundType soundType) {
+        super.playGallopSound(soundType);
         if (this.random.nextInt(10) == 0) {
             this.playSound(NaturalistSoundEvents.ZEBRA_BREATHE.get(), soundType.getVolume() * 0.6f, soundType.getPitch());
         }
     }
+
+    @Override
+    public double getPassengersRidingOffset() {
+        return super.getPassengersRidingOffset() + 0.25;
+    }
+
     @Override
     protected void playJumpSound() {
         this.playSound(NaturalistSoundEvents.ZEBRA_JUMP.get(), 0.4F, 1.0F);
