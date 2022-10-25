2bfa0531ce3e5fc0eccc9d9070cd8f8ac81ed318
SameButDifferent <76499872+samedifferent@users.noreply.github.com>
2022-10-24 20:29:50 -0400

    Add alligator egg

--- common/src/main/java/com/starfish_studios/naturalist/registry/NaturalistBlocks.java
+++ common/src/main/java/com/starfish_studios/naturalist/registry/NaturalistBlocks.java
@@ -1,6 +1,7 @@
 package com.starfish_studios.naturalist.registry;
 
 import com.starfish_studios.naturalist.Naturalist;
+import com.starfish_studios.naturalist.block.AlligatorEggBlock;
 import com.starfish_studios.naturalist.block.ChrysalisBlock;
 import com.starfish_studios.naturalist.block.TeddyBearBlock;
 import com.starfish_studios.naturalist.platform.CommonPlatformHelper;
@@ -18,6 +19,7 @@ import java.util.function.Supplier;
 public class NaturalistBlocks {
     public static final Supplier<Block> CHRYSALIS = CommonPlatformHelper.registerBlock("chrysalis", () -> new ChrysalisBlock(BlockBehaviour.Properties.of(Material.PLANT).randomTicks().strength(0.2F, 3.0F).sound(SoundType.GRASS).noOcclusion().noCollission()));
     public static final Supplier<Block> TEDDY_BEAR = registerBlock("teddy_bear", () -> new TeddyBearBlock(BlockBehaviour.Properties.of(Material.WOOL, MaterialColor.COLOR_BROWN).strength(0.8f).sound(SoundType.WOOL).noOcclusion()));
+    public static final Supplier<Block> ALLIGATOR_EGG = registerBlock("alligator_egg", () -> new AlligatorEggBlock(BlockBehaviour.Properties.copy(Blocks.TURTLE_EGG)));
 
     public static <T extends Block> Supplier<T> registerBlock(String name, Supplier<T> block) {
         Supplier<T> supplier = CommonPlatformHelper.registerBlock(name, block);
