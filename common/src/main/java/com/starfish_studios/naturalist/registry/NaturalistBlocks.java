8cf22f4bbfa42060dc897341fac3a6bc334efad7
crispytwig <lukehartinger@outlook.com>
2022-11-07 17:14:46 -0600

    [+~] Added Froglass textures, renamed Froglass colors in block registry, models, blockstates, and lang.

--- common/src/main/java/com/starfish_studios/naturalist/registry/NaturalistBlocks.java
+++ common/src/main/java/com/starfish_studios/naturalist/registry/NaturalistBlocks.java
@@ -21,9 +21,9 @@ public class NaturalistBlocks {
     public static final Supplier<Block> CHRYSALIS = CommonPlatformHelper.registerBlock("chrysalis", () -> new ChrysalisBlock(BlockBehaviour.Properties.of(Material.PLANT).randomTicks().strength(0.2F, 3.0F).sound(SoundType.GRASS).noOcclusion().noCollission()));
     public static final Supplier<Block> TEDDY_BEAR = registerBlock("teddy_bear", () -> new TeddyBearBlock(BlockBehaviour.Properties.of(Material.WOOL, MaterialColor.COLOR_BROWN).strength(0.8f).sound(SoundType.WOOL).noOcclusion()));
     public static final Supplier<Block> ALLIGATOR_EGG = registerBlock("alligator_egg", () -> new AlligatorEggBlock(BlockBehaviour.Properties.copy(Blocks.TURTLE_EGG)));
-    public static final Supplier<Block> BLUE_FROGLASS = registerBlock("blue_froglass", () -> new GlassBlock(BlockBehaviour.Properties.copy(Blocks.GLASS)));
-    public static final Supplier<Block> GREEN_FROGLASS = registerBlock("green_froglass", () -> new GlassBlock(BlockBehaviour.Properties.copy(Blocks.GLASS)));
-    public static final Supplier<Block> RED_FROGLASS = registerBlock("red_froglass", () -> new GlassBlock(BlockBehaviour.Properties.copy(Blocks.GLASS)));
+    public static final Supplier<Block> AZURE_FROGLASS = registerBlock("azure_froglass", () -> new GlassBlock(BlockBehaviour.Properties.copy(Blocks.GLASS)));
+    public static final Supplier<Block> VERDANT_FROGLASS = registerBlock("verdant_froglass", () -> new GlassBlock(BlockBehaviour.Properties.copy(Blocks.GLASS)));
+    public static final Supplier<Block> CRIMSON_FROGLASS = registerBlock("crimson_froglass", () -> new GlassBlock(BlockBehaviour.Properties.copy(Blocks.GLASS)));
 
     public static <T extends Block> Supplier<T> registerBlock(String name, Supplier<T> block) {
         Supplier<T> supplier = CommonPlatformHelper.registerBlock(name, block);
