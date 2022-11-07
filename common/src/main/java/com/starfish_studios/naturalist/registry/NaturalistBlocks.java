b3d22a780bbf9e034c1cac58eefa1c0d5f93290b
SameButDifferent <76499872+samedifferent@users.noreply.github.com>
2022-11-07 13:24:36 -0500

    Add froglass and loot tables

--- common/src/main/java/com/starfish_studios/naturalist/registry/NaturalistBlocks.java
+++ common/src/main/java/com/starfish_studios/naturalist/registry/NaturalistBlocks.java
@@ -9,6 +9,7 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.GlassBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
@@ -20,6 +21,9 @@ public class NaturalistBlocks {
     public static final Supplier<Block> CHRYSALIS = CommonPlatformHelper.registerBlock("chrysalis", () -> new ChrysalisBlock(BlockBehaviour.Properties.of(Material.PLANT).randomTicks().strength(0.2F, 3.0F).sound(SoundType.GRASS).noOcclusion().noCollission()));
     public static final Supplier<Block> TEDDY_BEAR = registerBlock("teddy_bear", () -> new TeddyBearBlock(BlockBehaviour.Properties.of(Material.WOOL, MaterialColor.COLOR_BROWN).strength(0.8f).sound(SoundType.WOOL).noOcclusion()));
     public static final Supplier<Block> ALLIGATOR_EGG = registerBlock("alligator_egg", () -> new AlligatorEggBlock(BlockBehaviour.Properties.copy(Blocks.TURTLE_EGG)));
+    public static final Supplier<Block> BLUE_FROGLASS = registerBlock("blue_froglass", () -> new GlassBlock(BlockBehaviour.Properties.copy(Blocks.GLASS)));
+    public static final Supplier<Block> GREEN_FROGLASS = registerBlock("green_froglass", () -> new GlassBlock(BlockBehaviour.Properties.copy(Blocks.GLASS)));
+    public static final Supplier<Block> RED_FROGLASS = registerBlock("red_froglass", () -> new GlassBlock(BlockBehaviour.Properties.copy(Blocks.GLASS)));
 
     public static <T extends Block> Supplier<T> registerBlock(String name, Supplier<T> block) {
         Supplier<T> supplier = CommonPlatformHelper.registerBlock(name, block);
